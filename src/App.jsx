9b1df7f69265c5516cf97cbbb87185889d27f407
mhebd <mhemonhossain@gmail.com>
2022-11-05 00:27:40 +0600

    App module bug fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import './assets/css/style.min.css';
+import img1 from './assets/images/Decore.png';
+import img2 from './assets/images/Ellipse 8.png';
 import Booking from './component/sections/booking/Booking';
 import Destination from './component/sections/destination/Destination';
 import Footer from './component/sections/footer/Footer';
@@ -12,8 +14,8 @@ import Testimonial from './component/sections/testimonial/Testimonial';
 function App() {
   return (
     <>
-      <img src="images/Decore.png" alt="" className="after" />
-      <img src="images/Ellipse 8.png" alt="" className="before" />
+      <img src={img1} alt="" className="after" />
+      <img src={img2} alt="" className="before" />
       <Hero />
 
       <Service />
